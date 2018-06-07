c8d9e7022aef1d22d6a06896a03e24765d2710f4
DaanWillems <dwillems2@avans.nl>
2018-06-07 23:00:10 +0200

    Wow

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	scribble "github.com/nanobox-io/golang-scribble"
 )
+
+var db *scribble.Driver
 
 func main() {
 	http.HandleFunc("/", indexHandler)
@@ -31,9 +35,41 @@ func cryptHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	if len(r.Form["message"][0]) > 0 {
-		w.Write([]byte((encrypt(r.Form["pass"][0], r.Form["message"][0]))))
+		result := []byte((encrypt(r.Form["pass"][0], r.Form["message"][0])))
+		saveMessage(r.Form["name"][0], string(result))
+		w.Write([]byte("Saved succesfully!"))
 	} else {
-		w.Write([]byte(decrypt(r.Form["pass"][0], "0zvJ3LiQCwltILKV3TTo3WgfBDiQokPk")))
+		w.Write([]byte(decrypt(r.Form["pass"][0], getMessage(r.Form["name"][0]))))
+	}
+}
+
+func getMessage(user string) string {
+	db, err := scribble.New("db", nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	message := Message{}
+	if err := db.Read("message", user, &message); err != nil {
+		fmt.Println("Error", err)
+	}
+
+	return message.Content
+}
+
+func saveMessage(user string, content string) {
+
+	db, err := scribble.New("db", nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	message := &Message{
+		Content: content,
+		User:    user,
+	}
+
+	if err := db.Write("message", user, message); err != nil {
+		fmt.Println("Error", err)
 	}
 }
 
@@ -58,7 +94,6 @@ func encrypt(key string, text string) string {
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 
-	// convert to base64
 	return base64.URLEncoding.EncodeToString(ciphertext)
 }
 
@@ -89,6 +124,11 @@ func createHash(key string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+type Message struct {
+	Content string
+	User    string
 }
 
 var htmlTemplate = `
